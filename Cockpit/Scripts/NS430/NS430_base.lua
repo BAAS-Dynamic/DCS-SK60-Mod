1e2b10330e51b655a7b0fbe5c157ba965cc780d7
CorsairCat <1274056321@qq.com>
2021-08-11 14:40:09 +0100

    update of new function

--- Cockpit/Scripts/NS430/NS430_base.lua
+++ Cockpit/Scripts/NS430/NS430_base.lua
@@ -90,8 +90,114 @@ bs430_base_surround.level                           = NS430_DEFAULT_NOCLIP_LEVEL
 bs430_base_surround.parent_element	                = "ns430_base_clip"
 Add(bs430_base_surround)
 
--- 225 * 156
+-- 225 * 156 line width = 41 2 55 2 55 2 100  
+-- = 440 * 304.7
 
+local bs430_base_surround 				            = CreateElement "ceTexPoly"
+bs430_base_surround.vertices                        = GPS_vert_gen(440, 390.625*aspect)
+bs430_base_surround.indices                         = {0,1,2,2,3,0}
+bs430_base_surround.tex_coords                      = tex_coord_gen(0,0,1024,512,2048,2048)
+bs430_base_surround.material                        = "EALT_BG_COLOR" -- blue_ns430_material
+bs430_base_surround.name 			                = create_guid_string()
+bs430_base_surround.init_pos                        = {-1 + 231.72/default_gps_x, 1*aspect - 218.7525*aspect/default_gps_x, 0}
+bs430_base_surround.init_rot		                = {0, 0, 0}
+bs430_base_surround.collimated	                    = true
+bs430_base_surround.element_params                  = {"NAVU_BASE_ENABLE"}              -- Initialize the main display control
+bs430_base_surround.controllers                     = {{"opacity_using_parameter",0}}
+bs430_base_surround.use_mipfilter                   = true
+bs430_base_surround.additive_alpha                  = true
+bs430_base_surround.h_clip_relation                 = h_clip_relations.COMPARE
+bs430_base_surround.level                           = NS430_DEFAULT_NOCLIP_LEVEL
+bs430_base_surround.parent_element	                = "ns430_base_clip"
+Add(bs430_base_surround)
+
+local bs430_base_surround 				            = CreateElement "ceTexPoly"
+bs430_base_surround.vertices                        = GPS_vert_gen(440, 218.775*aspect)
+bs430_base_surround.indices                         = {0,1,2,2,3,0}
+bs430_base_surround.tex_coords                      = tex_coord_gen(0,0,1024,512,2048,2048)
+bs430_base_surround.material                        = "EALT_BG_COLOR" -- blue_ns430_material
+bs430_base_surround.name 			                = create_guid_string()
+bs430_base_surround.init_pos                        = {-1 + 231.72/default_gps_x, 1*aspect - 520*aspect/default_gps_x, 0}
+bs430_base_surround.init_rot		                = {0, 0, 0}
+bs430_base_surround.collimated	                    = true
+bs430_base_surround.element_params                  = {"NAVU_BASE_ENABLE", "FREQ_FOCUS_COM"}              -- Initialize the main display control {0,50,255,255}
+bs430_base_surround.controllers                     = {{"opacity_using_parameter",0}, {"change_color_when_parameter_equal_to_number", 1, 1, 30/255,100/255,140/255}}
+bs430_base_surround.use_mipfilter                   = true
+bs430_base_surround.additive_alpha                  = true
+bs430_base_surround.h_clip_relation                 = h_clip_relations.COMPARE
+bs430_base_surround.level                           = NS430_DEFAULT_NOCLIP_LEVEL
+bs430_base_surround.parent_element	                = "ns430_base_clip"
+Add(bs430_base_surround)
+
+local BS430_COM_FREQ             = CreateElement "ceStringPoly" --Create a character output element "ceTexPoly" means to create a texture model
+BS430_COM_FREQ.material          = "BS430_font_white"    --FONT_             --Material type (note the font material created above)
+BS430_COM_FREQ.init_pos          = {-1 + 30.72/default_gps_x, 1*aspect - 440*aspect/default_gps_x}         -- This is the coordinates of the alignment point [this is the maximum limit of the current model (do not exceed when aligning the corners)]
+BS430_COM_FREQ.alignment         = "DownLeft"       --Alignment settings：Left/Right/Center; Top/Down/Center
+BS430_COM_FREQ.stringdefs        = {1.5*0.004,1.5 * 0.004, 0, 0}    --{ecrase vertical si inf a 0.01,ecrase lateral * streccth, 0, 0} The first value controls the width, the second value controls the height
+BS430_COM_FREQ.formats           = {"%03.0f.", "%s"}
+BS430_COM_FREQ.element_params    = {"COM_FREQ_ACT_1", "NAVU_BASE_ENABLE"} -- top left first line display
+BS430_COM_FREQ.controllers       = {{"text_using_parameter",0},{"opacity_using_parameter",1}}
+BS430_COM_FREQ.collimated        = true
+BS430_COM_FREQ.use_mipfilter     = true
+BS430_COM_FREQ.additive_alpha    = true
+BS430_COM_FREQ.isvisible		  = true
+BS430_COM_FREQ.h_clip_relation   = h_clip_relations.COMPARE -- INCREASE_IF_LEVEL-- --REWRITE_LEVEL
+BS430_COM_FREQ.level			  = NS430_DEFAULT_NOCLIP_LEVEL
+BS430_COM_FREQ.parent_element    = "ns430_base_clip"  --Parent node name - can bind parent nodes that are not on the same layer
+Add(BS430_COM_FREQ)
+
+local BS430_COM_FREQ             = CreateElement "ceStringPoly" --Create a character output element "ceTexPoly" means to create a texture model
+BS430_COM_FREQ.material          = "BS430_font_white"    --FONT_             --Material type (note the font material created above)
+BS430_COM_FREQ.init_pos          = {-1 + 281.72/default_gps_x, 1*aspect - 440*aspect/default_gps_x}         -- This is the coordinates of the alignment point [this is the maximum limit of the current model (do not exceed when aligning the corners)]
+BS430_COM_FREQ.alignment         = "DownRight"       --Alignment settings：Left/Right/Center; Top/Down/Center
+BS430_COM_FREQ.stringdefs        = {1.1*0.004,1.1 * 0.004, 0, 0}    --{ecrase vertical si inf a 0.01,ecrase lateral * streccth, 0, 0} The first value controls the width, the second value controls the height
+BS430_COM_FREQ.formats           = {"%03.0f", "%s"}
+BS430_COM_FREQ.element_params    = {"COM_FREQ_ACT_2", "NAVU_BASE_ENABLE"} -- top left first line display
+BS430_COM_FREQ.controllers       = {{"text_using_parameter",0},{"opacity_using_parameter",1}}
+BS430_COM_FREQ.collimated        = true
+BS430_COM_FREQ.use_mipfilter     = true
+BS430_COM_FREQ.additive_alpha    = true
+BS430_COM_FREQ.isvisible		 = true
+BS430_COM_FREQ.h_clip_relation   = h_clip_relations.COMPARE -- INCREASE_IF_LEVEL-- --REWRITE_LEVEL
+BS430_COM_FREQ.level			 = NS430_DEFAULT_NOCLIP_LEVEL
+BS430_COM_FREQ.parent_element    = "ns430_base_clip"  --Parent node name - can bind parent nodes that are not on the same layer
+Add(BS430_COM_FREQ)
+
+local line2_offset = 220
+
+local BS430_COM_FREQ             = CreateElement "ceStringPoly" --Create a character output element "ceTexPoly" means to create a texture model
+BS430_COM_FREQ.material          = "BS430_font_white"    --FONT_             --Material type (note the font material created above)
+BS430_COM_FREQ.init_pos          = {-1 + 30.72/default_gps_x, 1*aspect - (440 + line2_offset)*aspect/default_gps_x}         -- This is the coordinates of the alignment point [this is the maximum limit of the current model (do not exceed when aligning the corners)]
+BS430_COM_FREQ.alignment         = "DownLeft"       --Alignment settings：Left/Right/Center; Top/Down/Center
+BS430_COM_FREQ.stringdefs        = {1.5*0.004,1.5 * 0.004, 0, 0}    --{ecrase vertical si inf a 0.01,ecrase lateral * streccth, 0, 0} The first value controls the width, the second value controls the height
+BS430_COM_FREQ.formats           = {"%03.0f.", "%s"}
+BS430_COM_FREQ.element_params    = {"COM_FREQ_STB_1", "NAVU_BASE_ENABLE", "FREQ_FOCUS_COM"} -- top left first line display
+BS430_COM_FREQ.controllers       = {{"text_using_parameter",0},{"opacity_using_parameter",1},{"change_color_when_parameter_equal_to_number", 2, 1, 0/255,50/255,255/255}}
+BS430_COM_FREQ.collimated        = true
+BS430_COM_FREQ.use_mipfilter     = true
+BS430_COM_FREQ.additive_alpha    = true
+BS430_COM_FREQ.isvisible		  = true
+BS430_COM_FREQ.h_clip_relation   = h_clip_relations.COMPARE -- INCREASE_IF_LEVEL-- --REWRITE_LEVEL
+BS430_COM_FREQ.level			  = NS430_DEFAULT_NOCLIP_LEVEL
+BS430_COM_FREQ.parent_element    = "ns430_base_clip"  --Parent node name - can bind parent nodes that are not on the same layer
+Add(BS430_COM_FREQ)
+
+local BS430_COM_FREQ             = CreateElement "ceStringPoly" --Create a character output element "ceTexPoly" means to create a texture model
+BS430_COM_FREQ.material          = "BS430_font_white"    --FONT_             --Material type (note the font material created above)
+BS430_COM_FREQ.init_pos          = {-1 + 281.72/default_gps_x, 1*aspect - (440 + line2_offset)*aspect/default_gps_x}         -- This is the coordinates of the alignment point [this is the maximum limit of the current model (do not exceed when aligning the corners)]
+BS430_COM_FREQ.alignment         = "DownRight"       --Alignment settings：Left/Right/Center; Top/Down/Center
+BS430_COM_FREQ.stringdefs        = {1.1*0.004,1.1 * 0.004, 0, 0}    --{ecrase vertical si inf a 0.01,ecrase lateral * streccth, 0, 0} The first value controls the width, the second value controls the height
+BS430_COM_FREQ.formats           = {"%03.0f", "%s"}
+BS430_COM_FREQ.element_params    = {"COM_FREQ_STB_2", "NAVU_BASE_ENABLE", "FREQ_FOCUS_COM"} -- top left first line display
+BS430_COM_FREQ.controllers       = {{"text_using_parameter",0},{"opacity_using_parameter",1},{"change_color_when_parameter_equal_to_number", 2, 1, 0/255,50/255,255/255}}
+BS430_COM_FREQ.collimated        = true
+BS430_COM_FREQ.use_mipfilter     = true
+BS430_COM_FREQ.additive_alpha    = true
+BS430_COM_FREQ.isvisible		 = true
+BS430_COM_FREQ.h_clip_relation   = h_clip_relations.COMPARE -- INCREASE_IF_LEVEL-- --REWRITE_LEVEL
+BS430_COM_FREQ.level			 = NS430_DEFAULT_NOCLIP_LEVEL
+BS430_COM_FREQ.parent_element    = "ns430_base_clip"  --Parent node name - can bind parent nodes that are not on the same layer
+Add(BS430_COM_FREQ)
 -- Third Layer for Moving Map display
 
 -- "BS430_font_white"
