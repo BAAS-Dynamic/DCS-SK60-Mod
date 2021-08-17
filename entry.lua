8aae477d35676d095cd9720c7560fbc8197a7550
CorsairCat <1274056321@qq.com>
2021-08-17 22:59:54 +0100

    clear Cockpit and EFM for Ai mod use only

--- entry.lua
+++ entry.lua
@@ -1,7 +1,7 @@
 -- SAAB_SK60_FM
 -- local FM_dll=nil
 -- Test Collision only
-local FM_dll= "SAAB_SK60_FM.dll" -- "A-6E_Intruder_FM" --
+local FM_dll= nil
 
 self_ID = "SK-60"
 declare_plugin(self_ID,
@@ -64,57 +64,11 @@ mount_vfs_sound_path    (current_mod_path.."/Sounds")
 -------------------------------------------------------------------------------------
 local FM = nil
 
-test_susp = {
-	{
-		wheel_radius         = 0.45,
-		
-			arg_post             = 0,
-			arg_amortizer        = 1,
-			arg_wheel_rotation   = 76,
-			arg_wheel_yaw        = 2,
-			collision_shell_name = "WHEEL_F",
-		},
-		{
-			wheel_radius         = 0.77,
-	
-			arg_post             = 3,
-			arg_amortizer        = 4,
-			arg_wheel_rotation   = 77,
-			arg_wheel_yaw        = -1,
-			collision_shell_name = "WHEEL_R",
-		},
-		{
-			wheel_radius         = 0.77,
-	
-			arg_post             = 5,
-			arg_amortizer        = 6,
-			arg_wheel_rotation   = 77,
-			arg_wheel_yaw        = -1,
-			collision_shell_name = "WHEEL_L",
-		},
-}
-
-dofile(current_mod_path.."/suspension.lua")
-
-if FM_dll then
-	FM=
-	{
-		[1] = self_ID,
-		[2] = FM_dll,
-		center_of_mass = {0, 0, 0},--{5.8784 - 4.572, -0.7883, 0},
-		-- the moment_of_inertia is following the data from nasa
-		-- reverse the axis of y and z
-		moment_of_inertia = {12066.11, 44944.01, 30962.9, 1413.4},
-		suspension = suspension_data,
-	}
-else
-    FM=nil
-end
+--dofile(current_mod_path.."/suspension.lua")
 
 dofile(current_mod_path.."/sk-60_weapons.lua")
 dofile(current_mod_path.."/Views.lua")
 make_view_settings('SK-60', ViewSettings, SnapViews)
-make_flyable('SK-60',current_mod_path..'/Cockpit/Scripts/', FM , current_mod_path..'/Comm.lua')
 -------------------------------------------------------------------------------------
 dofile(current_mod_path..'/SK-60.lua')
 -------------------------------------------------------------------------------------
