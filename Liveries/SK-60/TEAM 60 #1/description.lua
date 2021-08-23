61c910817dcc76ccad2f09b75f69c0c1e862c15c
Breadmaker91 <europe_elmen@hotmail.com>
2021-08-23 14:19:16 +0200

    Texture update

--- Liveries/SK-60/TEAM 60 #1/description.lua
+++ Liveries/SK-60/TEAM 60 #1/description.lua
@@ -1,5 +1,6 @@
 livery = {
 	{"SK60_body2",		DIFFUSE				,	"SK60_body2", false};
 	{"HELMET BAKED",	DIFFUSE				,	"SK60_helmet", false};
+	{"Panel Board.001",	DIFFUSE				,	"SK60_dashboardplate", false};
 }
 name = "TEAM60 #1'"	
--- Liveries/SK-60/TEAM 60 #6/description.lua
+++ Liveries/SK-60/TEAM 60 #6/description.lua
@@ -1,5 +1,7 @@
 livery = {
 	{"SK60_body2",		DIFFUSE				,	"SK60_body2", false};
 	{"HELMET BAKED",	DIFFUSE				,	"SK60_helmet", false};
+	{"SK60_pilothead",	DIFFUSE				,	"SK60_pilothead", false};
+	{"Panel Board.001",	DIFFUSE				,	"SK60_dashboardplate", false};
 }
 name = "TEAM60 #6'"	
