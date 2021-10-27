f037de602a305f5bb37c58656f741095a92c4e1e
Breadmaker91 <europe_elmen@hotmail.com>
2021-10-27 19:38:16 +0200

    EDM & texture update

--- Liveries/SK-60/TEAM 60 #2/description.lua
+++ Liveries/SK-60/TEAM 60 #2/description.lua
@@ -3,5 +3,12 @@ livery = {
 	{"HELMET BAKED",	DIFFUSE				,	"SK60_helmet", false};
 	{"Panel Board.001",	DIFFUSE				,	"SK60_dashboardplate", false};
 	{"SK60_pilothead",	DIFFUSE				,	"SK60_pilothead", false};
+	{"SK60_PilotSuit",	DIFFUSE				,	"SK60_PilotGSuit", false};
+	{"SK60_PilotGSuit",	ROUGHNESS_METALLIC	,	"SK60_PilotGSuit_roughmet", false};
+	{"SK60_PilotGSuit",	NORMAL				,	"SK60_PilotGSuit_normal", false};
+	{"undersuit.001",	DIFFUSE				,	"SK60_PilotOverall", false};
+	{"undersuit.001",	ROUGHNESS_METALLIC	,	"SK60_PilotOverall_roughmet", false};
+	{"undersuit.001",	NORMAL				,	"SK60_PilotOverall_normal", false};
+	{"SK60_PilotPatch",	DIFFUSE				,	"SK60_Patch", false};
 }
 name = "TEAM60 #2'"	
