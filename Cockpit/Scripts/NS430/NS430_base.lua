3599f26ee341327d76601aa449286c0273fa3867
CorsairCat <1274056321@qq.com>
2022-02-02 04:15:01 +0000

    base usable moving map

--- Cockpit/Scripts/NS430/NS430_base.lua
+++ Cockpit/Scripts/NS430/NS430_base.lua
@@ -9,6 +9,7 @@ local half_height  = GetAspect() * half_width
 
 local aspect       = GetAspect()
 --default_gps_y = 1000 * aspect
+local map_scaler    = 0.02
 
 -- This is the top trim layer of the total instrument
 local ns430_base_clip 			 	    = CreateElement "ceMeshPoly" --This is the clipping layer
@@ -312,11 +313,11 @@ Add(BS430_COM_FREQ)
 local Moving_Map_Clip                   = CreateElement "ceMeshPoly" --This is the clipping layer
 Moving_Map_Clip.name 			        = "moving_map_clip"
 Moving_Map_Clip.primitivetype   	    = "triangles"
-Moving_Map_Clip.vertices 		        = { {0.7715, aspect * 0.92773}, {0.7715,-aspect * 0.92773}, {-0.7715,-aspect * 0.92773}, {-0.543,aspect * 0.92773},} --四个边角
+Moving_Map_Clip.vertices 		        = { {0.7715, aspect * 0.92773}, {0.7715,-aspect * 0.92773}, {-0.7715,-aspect * 0.92773}, {-0.7715,aspect * 0.92773},} --四个边角
 Moving_Map_Clip.indices 		        = {0,1,2,0,2,3}
 Moving_Map_Clip.init_pos		        = {0.2285, 0.0722344, 0}
 Moving_Map_Clip.init_rot		        = {0, 0, 0}
-Moving_Map_Clip.material		        = "DBG_GREEN"
+Moving_Map_Clip.material		        = "DBG_BLUE"
 Moving_Map_Clip.h_clip_relation         = h_clip_relations.INCREASE_IF_LEVEL --INCREASE_IF_LEVEL--COMPARE --REWRITE_LEVEL
 Moving_Map_Clip.level			        = NS430_DEFAULT_LEVEL - 1
 Moving_Map_Clip.isdraw		            = true
@@ -331,32 +332,32 @@ local eadi_adi_rotate                   = CreateElement "ceSimple"
 eadi_adi_rotate.name                    = "navu_moving_map_center"
 eadi_adi_rotate.init_pos                = {0, 0, 0}
 eadi_adi_rotate.element_params          = {"MAP_CENTER_Y", "MAP_CENTER_X"}
-eadi_adi_rotate.controllers             = {{"move_up_down_using_parameter",0,0.05},{"move_left_right_using_parameter",1,0.05}}
+eadi_adi_rotate.controllers             = {{"move_up_down_using_parameter",0,map_scaler},{"move_left_right_using_parameter",1,map_scaler}}
 eadi_adi_rotate.collimated	            = true
 eadi_adi_rotate.use_mipfilter           = true
 eadi_adi_rotate.additive_alpha          = true
 eadi_adi_rotate.h_clip_relation         = h_clip_relations.COMPARE
-eadi_adi_rotate.level                   = NS430_DEFAULT_LEVEL
-eadi_adi_rotate.parent_element	        = "navu_moving_map_center"
+eadi_adi_rotate.level                   = NS430_DEFAULT_LEVEL --NS430_DEFAULT_LEVEL
+eadi_adi_rotate.parent_element	        = "moving_map_clip"
 eadi_adi_rotate.isvisible               = false
 Add(eadi_adi_rotate)
 
 
 -- Moving map airport display, max 100 airports
 -- 310 * 310 in display -> equal to 1900 * 1900  360 * 1024 
-for i = 1, 100, 1 do
+for i = 0, 100, 1 do
     -- airport icon
     local airport_icon 				      = CreateElement "ceTexPoly"
     airport_icon.vertices                 = GPS_vert_gen(128,128)
     airport_icon.indices                  = {0,1,2,2,3,0}
     airport_icon.tex_coords               = tex_coord_gen(1536,0,128,128,2048,2048)
-    airport_icon.material                 = "DBG_GREY"--blue_ns430_material
+    airport_icon.material                 = "DBG_RED"--blue_ns430_material
     airport_icon.name 			          = create_guid_string()
     airport_icon.init_pos                 = {0, 0, 0}
     airport_icon.init_rot		          = {0, 0, 0}
     airport_icon.collimated	              = true
     airport_icon.element_params           = {"NS430_MAP_DISPLAY", "NAVU_BASE_ENABLE", "AIRPORT_ENABLE_"..i, "AIRPORT_LON_"..i, "AIRPORT_LAT_"..i}
-    airport_icon.controllers              = {{"opacity_using_parameter",0},{"opacity_using_parameter",1},{"opacity_using_parameter",2},{"move_up_down_using_parameter",3,0.05},{"move_left_right_using_parameter",4,0.05}}
+    airport_icon.controllers              = {{"opacity_using_parameter",0},{"opacity_using_parameter",1},{"opacity_using_parameter",2},{"move_up_down_using_parameter",4,map_scaler},{"move_left_right_using_parameter",3,map_scaler}}
     airport_icon.use_mipfilter            = true
     airport_icon.additive_alpha           = true
     airport_icon.h_clip_relation          = h_clip_relations.COMPARE
@@ -372,7 +373,7 @@ for i = 1, 100, 1 do
     airport_name.stringdefs        = {0.85 *0.002, 1 * 0.002, 0, 0}    --{ecrase vertical si inf a 0.01,ecrase lateral * streccth, 0, 0} The first value controls the width, the second value controls the height
     airport_name.formats           = {"%s", "%s"}
     airport_name.element_params    = {"NS430_MAP_DISPLAY", "NAVU_BASE_ENABLE", "AIRPORT_ENABLE_"..i, "AIRPORT_LON_"..i, "AIRPORT_LAT_"..i, "AIRPORT_NAME_"..i} -- top left first line display
-    airport_name.controllers       = {{"opacity_using_parameter",0},{"opacity_using_parameter",1},{"opacity_using_parameter",2},{"move_up_down_using_parameter",3,0.05},{"move_left_right_using_parameter",4,0.05},{"text_using_parameter",5}}
+    airport_name.controllers       = {{"opacity_using_parameter",0},{"opacity_using_parameter",1},{"opacity_using_parameter",2},{"move_up_down_using_parameter",4,map_scaler},{"move_left_right_using_parameter",3,map_scaler},{"text_using_parameter",5}}
     airport_name.collimated        = true
     airport_name.use_mipfilter     = true
     airport_name.additive_alpha    = true
