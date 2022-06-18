04679016d3e080dc7c438fdcb7263895f6186647
CorsairCat <1274056321@qq.com>
2022-06-18 20:48:24 +0100

    new trying for enable the clip size change

--- Cockpit/Scripts/NS430/IndicatorA/NS430_nav_map_page.lua
+++ Cockpit/Scripts/NS430/IndicatorA/NS430_nav_map_page.lua
@@ -2,8 +2,7 @@
 -- 790 * 475 // 1024 * 512
 local Moving_Map_Clip                   = CreateElement "ceMeshPoly" --This is the clipping layer
 Moving_Map_Clip.name 			        = "moving_map_clip"
-Moving_Map_Clip.primitivetype   	    = "triangles"
-Moving_Map_Clip.vertices 		        = { {0.7715, aspect * 0.92773}, {0.7715,-aspect * 0.92773}, {-0.7715,-aspect * 0.92773}, {-0.7715,aspect * 0.92773},} --四个边角
+Moving_Map_Clip.vertices 		        = { {0.7715-0.29297*2, aspect * 0.92773}, {0.7715-0.29297*2,-aspect * 0.92773}, {-0.7715,-aspect * 0.92773}, {-0.7715,aspect * 0.92773},} --四个边角
 Moving_Map_Clip.indices 		        = {0,1,2,0,2,3}
 Moving_Map_Clip.init_pos		        = {0.2285, 0.0722344, 0}
 Moving_Map_Clip.init_rot		        = {0, 0, 0}
@@ -14,6 +13,25 @@ Moving_Map_Clip.isdraw		            = true
 Moving_Map_Clip.change_opacity          = false
 Moving_Map_Clip.element_params          = {"NS430_MAP_DISPLAY"}              -- Initialize the main display control
 Moving_Map_Clip.controllers             = {{"opacity_using_parameter",0}}
+Moving_Map_Clip.parent_element	        = "base_clip"
+Moving_Map_Clip.isvisible		        = SHOW_MASKS
+Add(Moving_Map_Clip)
+
+-- second clip
+local Moving_Map_Clip                   = CreateElement "ceMeshPoly" --This is the clipping layer
+Moving_Map_Clip.name 			        = "overlayer_clip"
+Moving_Map_Clip.vertices 		        = { {0.29297, aspect * 0.92773}, {0.29297,-aspect * 0.92773}, {-0.29297,-aspect * 0.92773}, {-0.29297,aspect * 0.92773},} --四个边角
+Moving_Map_Clip.indices 		        = {0,1,2,0,2,3}
+Moving_Map_Clip.init_pos		        = {0.70703-0.2285, 0, 0}
+Moving_Map_Clip.init_rot		        = {0, 0, 0}
+Moving_Map_Clip.material		        = "DBG_BLUE"
+Moving_Map_Clip.h_clip_relation         = h_clip_relations.INCREASE_IF_LEVEL --INCREASE_IF_LEVEL--COMPARE --REWRITE_LEVEL
+Moving_Map_Clip.level			        = NS430_DEFAULT_LEVEL - 1
+Moving_Map_Clip.isdraw		            = true
+Moving_Map_Clip.change_opacity          = false
+Moving_Map_Clip.element_params          = {"NS430_MAP_DISPLAY", "NS430_OVERLAYER_RIGHT"}              -- Initialize the main display control
+Moving_Map_Clip.controllers             = {{"opacity_using_parameter",0}, {"move_left_right_using_parameter",1,1}}
+Moving_Map_Clip.parent_element	        = "moving_map_clip"
 Moving_Map_Clip.isvisible		        = SHOW_MASKS
 Add(Moving_Map_Clip)
 
