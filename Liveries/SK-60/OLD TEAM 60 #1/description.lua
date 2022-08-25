a41ce9e43a1c20919c82d9fad1b10b0e96e3892d
Breadmaker91 <europe_elmen@hotmail.com>
2022-08-25 10:08:56 +0200

    Cockpit Fixes
    - Adjusted animations
    - Removed old Team 60 livery and replaced with new (and also optimized livery space)

--- Liveries/SK-60/OLD TEAM 60 #1/description.lua
+++ Liveries/SK-60/OLD TEAM 60 #1/description.lua
@@ -10,7 +10,12 @@ livery = {
 	{"SK60_GearAndAirbrake",	ROUGHNESS_METALLIC	,	"SK60_GearAndAirbrake_roughmet", 		false};
 	{"SK60_TailFins",			DIFFUSE				,	"SK60_TailFin", 						false};
 	{"SK60_TailFins",			ROUGHNESS_METALLIC	,	"SK60_TailFin_roughmet", 				false};
-	{"HELMET BAKED",	DIFFUSE				,	"SK60_helmet", false};
+	{"SK60_Helmet",				DIFFUSE				,	"SK60_helmet", false};
+	{"SK60_Helmet",				ROUGHNESS_METALLIC	,	"SK60_helmet_roughmet", false};
+	{"SK60_Helmet",				NORMAL				,	"SK60_helmet_normal", false};
+	{"SK60_Mask",				DIFFUSE				,	"SK60_mask", false};
+	{"SK60_Mask",				ROUGHNESS_METALLIC	,	"SK60_mask_roughmet", false};
+	{"SK60_Mask",				NORMAL				,	"SK60_mask_normal", false};
 	{"Panel Board.001",	DIFFUSE				,	"SK60_dashboardplate", false};
 	{"SK60_PilotSuit",	DIFFUSE				,	"SK60_PilotGSuit", false};
 	{"SK60_PilotGSuit",	ROUGHNESS_METALLIC	,	"SK60_PilotGSuit_roughmet", false};
@@ -19,6 +24,8 @@ livery = {
 	{"undersuit.001",	ROUGHNESS_METALLIC	,	"SK60_PilotOverall_roughmet", false};
 	{"undersuit.001",	NORMAL				,	"SK60_PilotOverall_normal", false};
 	{"SK60_PilotPatch",	DIFFUSE				,	"SK60_Patch", false};
+	{"SK60_pilothead",	DIFFUSE				,	"SK60_pilothead", false};
+	
 	
 	--BORT NUMBERS---------------------------------------------------------------	
 	{"SK60_BortNumber_0", 0 ,"SK60_BortNumbers",false};
