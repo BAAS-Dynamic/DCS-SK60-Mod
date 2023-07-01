255aa2a9b4188c6ee50117c03f05806fc5571638
Breadmaker91 <europe_elmen@hotmail.com>
2023-07-01 08:44:47 +0200

    Cosmetic Update

---
+++ Liveries/SK-60/60 Years Anniversary/description.lua
@@ -0,0 +1,31 @@
+livery = {
+	{"SK60_body1",		DIFFUSE				,	"SK60_body1", false};
+	{"SK60_body1",		ROUGHNESS_METALLIC	,	"SK60_body1_roughmet", false};
+	{"SK60_body1",		NORMAL				,	"SK60_body1_normal", false};
+	{"SK60_body2",		DIFFUSE				,	"SK60_body2", false};
+	{"SK60_body2",		ROUGHNESS_METALLIC	,	"SK60_body2_roughmet", false};
+	{"SK60_body2",		NORMAL				,	"SK60_body2_normal", false};
+	{"SK60_Body3.003",		DIFFUSE				,	"SK60_body3", false};
+	{"SK60_Body3.003",		ROUGHNESS_METALLIC	,	"SK60_body3_roughmet", false};
+	{"SK60_Body3.003",		NORMAL				,	"SK60_body3_normal", false};	
+	{"SK60_gear_doors",			DIFFUSE				,	"SK60_gear_doors", 			false};
+	{"SK60_gear_doors",				ROUGHNESS_METALLIC	,	"SK60_gear_doors_roughmet", 		false};	
+	{"SK60_GearAndAirbrake",	DIFFUSE				,	"SK60_GearAndAirbrake", 	false};
+	{"SK60_GearAndAirbrake",	ROUGHNESS_METALLIC	,	"SK60_GearAndAirbrake_roughmet", 		false};
+	{"SK60_TailFins",			DIFFUSE				,	"SK60_TailFin", 						false};
+	{"SK60_TailFins",			ROUGHNESS_METALLIC	,	"SK60_TailFin_roughmet", 				false};
+	{"SK60_TailFins",			NORMAL				,	"SK60_TailFin_normal", 				false};
+	{"SK60_pilothead",	DIFFUSE				,	"SK60_pilothead", false};
+	{"SK60_WingTips",			DIFFUSE				,	"SK60_WingTips", 						false};
+	{"SK60_WingTips",			ROUGHNESS_METALLIC	,	"SK60_WingTips_roughmet", 				false};
+	
+	
+	--BORT NUMBERS---------------------------------------------------------------	
+	{"SK60_BortNumber_0", 0 ,"SK60_BortNumbers",false};
+	--{"SK60_BortNumber_0", DECAL ,"empty",false};
+	{"SK60_BortNumber_10", 0 ,"SK60_BortNumbers",false};
+	--{"SK60_BortNumber_10", DECAL ,"empty",false};
+	{"SK60_BortNumber_100", 0 ,"SK60_BortNumbers",false};
+	--{"SK60_BortNumber_100", DECAL ,"empty",false};
+}
+name = "60 Years Anniversary"	
--- Missions/QuickStart/quickstart.lua
+++
@@ -1,7 +0,0 @@
-planes = {
-    { 
-    name = _('SK60 Free Flight'),
-    file = 'SK60_Morning Free Flight Caucasus.miz',
-    },	
-}
-	 
